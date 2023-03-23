867b18a921315b02dba6001e0367f35b4ecf9971
NotableDeveloper <castlereal01@gmail.com>
2023-03-23 22:05:38 +0900

    DFS를 이용한 풀이 작성 DFS를 이용하여 조합을 구하는 풀이 작성

--- src/main/java/fossil/study/two_pointer/BOJ1940.java
+++ src/main/java/fossil/study/two_pointer/BOJ1940.java
@@ -4,6 +4,28 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class BOJ1940 {
+    static int n, m, count = 0;
+    static int[] materials, combi;
+
+    private static void DFS(int level, int start){
+        if(level == 2){
+            int sum = 0;
+
+            for(int i : combi) {
+                sum += i;
+            }
+
+            if(sum == m) count++;
+        }
+
+        else{
+            for(int i = start; i < n; i++){
+                combi[level] = materials[i];
+                DFS(level + 1, i + 1);
+            }
+        }
+    }
+
     private static int solution(int n, int m, int[] materials) {
         //lt : 배열의 좌측에서 시작, rt : 배열의 우측에서 시작
         int answer = 0, lt = 0, rt = n - 1;
@@ -40,16 +62,18 @@ public class BOJ1940 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] materials = new int[n];
+        n = sc.nextInt();
+        m = sc.nextInt();
+        materials = new int[n];
+        combi = new int[2];
 
         for(int i = 0; i < n; i++){
             materials[i] = sc.nextInt();
         }
 
-        System.out.println(solution(n, m, materials));
+        DFS(0, 0);
+        System.out.println(count);
+
+        //System.out.println(solution(n, m, materials));
     }
-
-
 }
