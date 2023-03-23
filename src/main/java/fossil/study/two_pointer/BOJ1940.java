95b904746811f4d4fb49d26dc46efbd245c974b8
NotableDeveloper <castlereal01@gmail.com>
2023-03-23 14:11:16 +0900

    주석 작성 풀이의 이해를 위한 주석 작성

--- src/main/java/fossil/study/two_pointer/BOJ1940.java
+++ src/main/java/fossil/study/two_pointer/BOJ1940.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class BOJ1940 {
     private static int solution(int n, int m, int[] materials) {
+        //lt : 배열의 좌측에서 시작, rt : 배열의 우측에서 시작
         int answer = 0, lt = 0, rt = n - 1;
 
         Arrays.sort(materials);
@@ -12,16 +13,23 @@ public class BOJ1940 {
         while(lt < rt){
             int sum = materials[lt] + materials[rt];
 
+            /*
+                좌, 우의 합이 같으면
+                좌측 포인터는 이동하고, 우측 포인터는 맨 우측으로
+                이동한다.
+             */
             if(sum == m){
                 answer++;
                 lt++;
                 rt = n - 1;
             }
 
+            // 좌, 우의 합이 찾는 값보다 크면 좌측만 이동
             else if(sum < m){
                 lt++;
             }
 
+            // 좌, 우의 합이 찾는 값보다 작으면 우측만 이동
             else if(sum > m){
                 rt--;
             }
