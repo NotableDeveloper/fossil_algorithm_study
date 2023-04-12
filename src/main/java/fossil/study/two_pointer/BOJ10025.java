775c7e6c066b21c2c015b51cd3cbab3f6edeb377
NotableDeveloper <castlereal01@gmail.com>
2023-04-12 16:24:20 +0900

    Camel case 적용 리뷰 내용을 반영하여 snake case가 적용된 변수를 camel case를 적용.

--- src/main/java/fossil/study/two_pointer/BOJ10025.java
+++ src/main/java/fossil/study/two_pointer/BOJ10025.java
@@ -6,13 +6,13 @@ public class BOJ10025 {
     // 앨버트의 우리 안에 존재할 수 있는 양동이의 수
     public static int bucket = 1000001;
     private static int solution(int k, int[] home){
-        // window_length = 좌에서 k만큼, 우에서 k만큼의 길이를 갖도록 하는 윈도우
-        int answer = 0, sum = 0, window_length = k * 2 + 1;
+        // windowLength = 좌에서 k만큼, 우에서 k만큼의 길이를 갖도록 하는 윈도우
+        int answer = 0, sum = 0, windowLength = k * 2 + 1;
 
         for(int i = 0; i < bucket; i++){
             // window 길이만큼 탐색을 했으면 크기 계산
-            if(i >= window_length){
-                sum -= home[i - window_length];
+            if(i >= windowLength){
+                sum -= home[i - windowLength];
             }
 
             // window 크기 누적
